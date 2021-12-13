3c2ea366c34f44a4e8cf14a48c45a64291580a82
Irfanmj <95951833+Irfanmj@users.noreply.github.com>
2021-12-13 22:39:31 +0800

    Update car.java

--- src/eduhelp/serv/car.java
+++ src/eduhelp/serv/car.java
@@ -52,6 +52,12 @@ public class car extends HttpServlet {
         String custAddress = request.getParameter("custAddress"); 
         //String fileLicense = request.getParameter("fileLicense"); 
         try {
+		
+		Class.forName("org.postgresql.Driver");
+
+            Connection conn = DriverManager.getConnection("jdbc:postgresql://ec2-54-204-128-96.compute-1.amazonaws.com:5432/dclgjbnt12qtde","bxatgjnyrydewa","57c8fed2ad071c2ff13fc104aba11684990d8cd93839b01631db30ea46640cb3");
+
+       
 
            Statement st = conn.createStatement();
 
